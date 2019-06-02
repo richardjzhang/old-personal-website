2a8c1b3eea705eb4412db8c37c906577654aba6a
richardjzhang <rzhang@mathspace.com.au>
2019-06-02 18:01:32 +1000

    Convert Panel to function

--- src/components/Panel/index.jsx
+++ src/components/Panel/index.jsx
@@ -4,34 +4,29 @@ import React, { type Node } from 'react';
 type Props = {|
   height: string,
   backgroundColor: string,
-  children?: Node,
+  children?: Node
 |};
 
-class Panel extends React.Component<Props> {
-  render() {
-    const { height, backgroundColor, children } = this.props;
-    return (
-      <React.Fragment>
-        <div
-          className="container"
-          style={{
-            height,
-            backgroundColor,
-          }}
-        >
-          {children}
-        </div>
-        <style jsx>
-          {`
-            .container {
-              padding: 24px 56px;
-              flex: 1;
-            }
-          `}
-        </style>
-      </React.Fragment>
-    );
-  }
-}
+const Panel = ({ height, backgroundColor, children }: Props) => (
+  <React.Fragment>
+    <div
+      className="container"
+      style={{
+        height,
+        backgroundColor
+      }}
+    >
+      {children}
+    </div>
+    <style jsx>
+      {`
+        .container {
+          padding: 24px 56px;
+          flex: 1;
+        }
+      `}
+    </style>
+  </React.Fragment>
+);
 
 export default Panel;
