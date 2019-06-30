d3918ff43c6f7752ac2ebe6ad434d83675fc6523
richardjzhang <rzhang@mathspace.com.au>
2019-07-01 01:02:02 +1000

    Go back to webfontloader

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import WebFont from 'webfontloader';
 
 import Landing from './views/Landing';
 import './index.css';
+
+WebFont.load({
+  google: {
+    families: ['Montserrat', 'sans-serif']
+  }
+});
 
 const root = document.getElementById('root');
 if (root !== null) {
